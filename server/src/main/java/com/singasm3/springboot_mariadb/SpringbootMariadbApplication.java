237af208b5aba3ed2762253b7438bcfb1c9063b6
Tung Nguyen <tungnd.goat@gmail.com>
2024-07-18 10:13:25 +0700

    init

---
+++ server/src/main/java/com/singasm3/springboot_mariadb/SpringbootMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.singasm3.springboot_mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMariadbApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/singasm3/springboot_mariadb/SpringbootMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singasm3.springboot_mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMariadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
